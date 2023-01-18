5ef472f63349b4734955a79e002237e4a1438f72
Dustin Zook <dustin.w.zook@gmail.com>
2023-01-18 16:03:51 -0600

    comleted repositories exercise

--- src/main/java/com/codeup/blogapp/controllers/PostController.java
+++ src/main/java/com/codeup/blogapp/controllers/PostController.java
@@ -4,10 +4,7 @@ import com.codeup.blogapp.models.Post;
 import com.codeup.blogapp.repositories.PostRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 
 import java.util.List;
@@ -39,11 +36,21 @@ public class PostController {
 //        return "view create post form";
 //    }
 //
-//    @PostMapping (path = "/posts/create")
-//    @ResponseBody
-//    public String adPage(){
-//        return "info from post request";
-//    }
+    @GetMapping ("/posts/create")
+    public String createAdPage(){
+        return "posts/create";
+    }
+
+    @PostMapping("/posts/create")
+    public String createAd(@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+        Post post = new Post();
+        post.setTitle(title);
+        post.setBody(body);
+
+        postDoa.save(post);
+
+        return "redirect:/posts";
+    }
 
 
 }
