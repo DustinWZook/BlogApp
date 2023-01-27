82ed334cb49273f14a6704efb35c89910f22f461
Dustin Zook <dustin.w.zook@gmail.com>
2023-01-27 16:31:53 -0600

    afternoon push

--- src/main/java/com/codeup/blogapp/controllers/PostController.java
+++ src/main/java/com/codeup/blogapp/controllers/PostController.java
@@ -9,16 +9,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 
+import java.text.AttributedString;
 import java.util.List;
 
 @Controller
 public class PostController {
 
-    private final PostRepository postDoa;
-    private final UserRepository userDoa;
-    public PostController(PostRepository postDoa, UserRepository userDoa){
-        this.postDoa = postDoa;
-        this.userDoa = userDoa;
+    private final PostRepository postDao;
+    private final UserRepository userDao;
+    public PostController(PostRepository postDao, UserRepository userDao){
+        this.postDao = postDao;
+        this.userDao = userDao;
     }
 
 
@@ -26,33 +27,60 @@ public class PostController {
 
     @GetMapping("/posts")
     public String getPosts(Model model){
-        List<Post> posts = postDoa.findAll();
+        List<Post> posts = postDao.findAll();
         model.addAttribute("posts", posts);
         return "posts/show";
     }
 
     @GetMapping("/posts/{id}")
     public String postPage(@PathVariable long id, Model model){
-        Post currentPost = postDoa.getReferenceById(id);
+        Post currentPost = postDao.getReferenceById(id);
         model.addAttribute("post", currentPost);
         return "posts/post";
     }
 
-    @GetMapping ("/posts/create")
-    public String createAdPage(){
+//    @GetMapping ("/posts/create")
+//    public String createAdPage(){
+//        return "posts/create";
+//    }
+//
+//    @PostMapping("/posts/create")
+//    public String createPost(@RequestParam(name = "username") String username,@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
+//        Post post = new Post();
+//        User user = userDao.findByUsername(username);
+//        post.setTitle(title);
+//        post.setBody(body);
+//        post.setUser(user);
+//        postDao.save(post);
+//        return "redirect:/posts";
+//    }
+
+    @GetMapping("/posts/create")
+    public String createAdPage(Model model){
+        model.addAttribute("post", new Post());
         return "posts/create";
     }
 
     @PostMapping("/posts/create")
-    public String createAd(@RequestParam(name = "username") String username,@RequestParam(name = "title") String title, @RequestParam(name = "body") String body){
-        Post post = new Post();
-        User user = userDoa.findByUsername(username);
-        post.setTitle(title);
-        post.setBody(body);
-        post.setUser(user);
-        postDoa.save(post);
+    public String createPost(@ModelAttribute Post post){
+        postDao.save(post);
         return "redirect:/posts";
     }
+
+    @GetMapping("/posts/{id}/edit/")
+    public String getEditPost(@PathVariable Long id, Model model){
+//        long postId = Long.parseLong(id);
+        Post post = postDao.getReferenceById(id);
+        model.addAttribute("post", post);
+        return "redirect:/posts/edit";
+    }
+
+//    @PostMapping("/posts/edit/{id}")
+//    public String editPost(@ModelAttribute Post post, @PathVariable("id") long id){
+//        Post updatePost = postDao.getReferenceById(id);
+//        postDao.save(post);
+//        return "redirect:/posts";
+//    }
 
 
 }
